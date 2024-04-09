6ae8268421e85946360d9013fab07ce70f7bb8c6
plms <wuyangzerg@gmail.com>
2024-04-09 15:26:04 +0800

    the first "Test Code Refactor"

--- TDD-Demo/src/main/java/com/tdd/demo/EmailTemplate.java
+++ TDD-Demo/src/main/java/com/tdd/demo/EmailTemplate.java
@@ -1,15 +1,22 @@
 package com.tdd.demo;
 
 public class EmailTemplate {
-    public EmailTemplate(String templateText) {
 
+    private String variableValue;
+    private String templateText;
+
+    public EmailTemplate(String templateText) {
+        this.templateText = templateText;
     }
 
-    public void set(String name, String reader) {
-
+    public void set(String variable, String value) {
+        this.variableValue = value;
     }
 
     public String evaluate() {
-        return null;
+        //return "Hello, Reader";   //hard codes. Step 2
+        //return "Hello, " + variableValue;   //Step 4
+        return this.templateText.replaceAll("\\$\\{name\\}", variableValue);   //Step 5
+
     }
 }
--- TDD-Demo/src/test/java/com/tdd/demo/TestTemplate.java
+++ TDD-Demo/src/test/java/com/tdd/demo/TestTemplate.java
@@ -1,22 +1,55 @@
 package com.tdd.demo;
 
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TestTemplate {
 
     /**
+     *  Test --> Code --> Refactor
+     * */
+
+    /**
+     * The first test.
      * 1, Write test first and then write the codes of EmailTemplate.
+     * 2, To make the test pass as quickly as possible by writing hard codes in EmailTemplate.java.
+     * 3, Write the second test and try different input and then we can see that the second test failed as we expected. Of course.
+     *    This technique is aptly named triangulation.
+     * 4, Keep on triangulating. To pass the second test with minimal effort we should use a variable to replace "Reader".
+     * 5, Write the third test to use "Hi" instead of "Hello". Presumably, the test will fail.
+     *    Add more faking details to pass the test.
      * */
     @Test
     public void oneVariable() throws Exception {
 
         EmailTemplate template = new EmailTemplate("Hello, ${name}");
         template.set("name", "Reader");
-        Assert.assertEquals("Hello, Reader", template.evaluate());
+        assertEquals("Hello, Reader", template.evaluate());
 
     }
 
+    /**
+     * The second test.
+     * */
+    @Test
+    public void differentVariable() throws Exception {
+        EmailTemplate template = new EmailTemplate("Hello, ${name}");
+        template.set("name", "someone else");
+        assertEquals("Hello, someone else", template.evaluate());
+
+    }
+
+    /**
+     * The third test.
+     * */
+    @Test
+    public void differentVariableAndText() throws Exception {
+        EmailTemplate template = new EmailTemplate("Hi, ${name}");
+        template.set("name", "someone else");
+        assertEquals("Hi, someone else", template.evaluate());
+
+    }
 
 }
