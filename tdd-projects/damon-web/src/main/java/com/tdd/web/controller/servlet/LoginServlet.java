5dc0d9c80862fac622b585642a7962cbe6f8de5c
plms <wuyangzerg@gmail.com>
2024-05-17 10:28:56 +0800

    test-driving web components, servlet: refactor the second test.

--- tdd-projects/damon-web/src/main/java/com/tdd/web/controller/servlet/LoginServlet.java
+++ tdd-projects/damon-web/src/main/java/com/tdd/web/controller/servlet/LoginServlet.java
@@ -1,5 +1,7 @@
 package com.tdd.web.controller.servlet;
 
+import com.tdd.web.service.AuthenticationService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,21 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.sendRedirect("/invalidLogin");   // hard coded
+        String username = request.getParameter("j_username");
+        String password = request.getParameter("j_password");
+        AuthenticationService authenticator = getAuthenticationService();
+        boolean validLogin = authenticator.isValidLogin(username, password);
+        if (validLogin) {
+            response.sendRedirect("/frontpage");
+            request.getSession().setAttribute("username", username);
+        } else {
+            response.sendRedirect("/invalidLogin");   // hard coded
+            // The simulation of storing username and password.
+        }
     }
+
+    protected AuthenticationService getAuthenticationService() {
+        return null;
+    }
+
 }
--- tdd-projects/damon-web/src/test/java/com/tdd/web/controller/servlet/TestLoginServlet.java
+++ tdd-projects/damon-web/src/test/java/com/tdd/web/controller/servlet/TestLoginServlet.java
@@ -1,5 +1,7 @@
 package com.tdd.web.controller.servlet;
 
+import com.tdd.web.service.AuthenticationService;
+import com.tdd.web.service.FakeAuthenticationService;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -11,7 +13,8 @@ import static org.junit.Assert.assertEquals;
 public class TestLoginServlet {
 
     /**
-     * 1, To test whether it will be to redirect to the error page if a use input a wrong password.
+     * 1, First test.
+     * To test whether it will be to redirect to the error page if a use input a wrong password.
      *
      * Remember that "test, code, refactor".
      * */
@@ -30,11 +33,13 @@ public class TestLoginServlet {
     }
 
     /**
-     * The second test. It is to force use to rewrite the logic in service.
-     * 2, To test if a valid user login in with correct password will be redirected to the front page.
+     * 2, The second test. It forces us to rewrite the logic in service in order to treat invalid and valid users.
+     * To test if a valid user login in with correct password will be redirected to the front page.
      * At this point in time the return value is hard coded in the service method of LoginServlet so
      * it is definitely not correct when a valid user is trying to login.
      * We should write some logic in the service method to pass the second test.
+     *
+     * In case of reviewing code in the future I keep this test failed and rewrite code in another test.
      * */
     @Test
     public void validLoginForwardsToFrontPageAndStoresUsername() throws Exception {
@@ -49,4 +54,39 @@ public class TestLoginServlet {
         assertEquals("validuser", request.getSession().getAttribute("username"));
 
     }
+
+    /**
+     * Rewrite the second test above.
+     * The test is passed with flying colours, but the first test failed because we didn't override the
+     * getAuthenticationService in it.
+     * */
+    @Test
+    public void rewriteTestOfValidLogin() throws Exception {
+        final String validUsername = "validuser";
+        final String correctPassword = "correctpassword";
+
+        // Configure fake AuthenticationService.
+        final FakeAuthenticationService authenticator = new FakeAuthenticationService();
+        authenticator.addUser(validUsername, correctPassword);
+
+        LoginServlet servlet = new LoginServlet() {
+            // Use fake instead of real thing. Namely return a fake "authenticator".
+            @Override
+            protected AuthenticationService getAuthenticationService() {
+                return authenticator;
+            }
+        };
+
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/login");
+        request.addParameter("j_username", "validuser");
+        request.addParameter("j_password", "correctpassword");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        servlet.service(request, response);
+
+        assertEquals("/frontpage", response.getRedirectedUrl());
+        assertEquals("validuser", request.getSession().getAttribute("username"));
+
+    }
+
+
 }
