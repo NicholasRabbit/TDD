0a82482b4bcf943e49b3c61388aae21ae146acff
plms <wuyangzerg@gmail.com>
2024-07-22 11:45:53 +0800

    TDD in Practice: Refactoring SingleValueOptionParser by extracting methods of conditions in 'if()'

--- tdd-in-practice/ann-arguments/src/main/java/com/tdd/practice/refactor/parser_refactored/SingleValueOptionParser.java
+++ tdd-in-practice/ann-arguments/src/main/java/com/tdd/practice/refactor/parser_refactored/SingleValueOptionParser.java
@@ -32,18 +32,30 @@ public class SingleValueOptionParser<T> implements OptionParserRefactored<T>{
         if (index == -1)
             return defaultValue;
 
-        if (index + 1 == arguments.size()
-                || arguments.get(index + 1).startsWith("-")) {
+        if (reachEnd(arguments, index)
+                || followedByAnotherArgument(arguments, index)) {
             throw new InsufficientArgumentsException(option.value());
         }
 
-        if (index + 2 < arguments.size()
-                && !arguments.get(index + 2).startsWith("-")) {
+        if (tooManyArguments(arguments, index)) {
             throw new TooManyArgumentsException(option.value());
         }
 
         String value = arguments.get(index + 1);
         return parseValue(value);
+    }
+
+    private boolean tooManyArguments(List<String> arguments, int index) {
+        return index + 2 < arguments.size()
+                && !arguments.get(index + 2).startsWith("-");
+    }
+
+    private boolean followedByAnotherArgument(List<String> arguments, int index) {
+        return arguments.get(index + 1).startsWith("-");
+    }
+
+    private boolean reachEnd(List<String> arguments, int index) {
+        return index + 1 == arguments.size();
     }
 
     protected T parseValue(String value) {
